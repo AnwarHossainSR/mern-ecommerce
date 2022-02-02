5c4c9da49da08e253d8689bbc0a1820c5ccd8f14
AnwarHossainSR <mahedianwar@gmail.com>
2022-02-02 20:54:03 +0600

    Register method

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,15 +2,10 @@ const ErrorHander = require("../utils/errorhander");
 const catchAsyncErrors = require("../middleware/catchAsyncErrors");
 const User = require("../models/userModel");
 const crypto = require("crypto");
+const sendToken = require("../utils/jwtToken");
 
 // Register a User
 exports.registerUser = catchAsyncErrors(async (req, res, next) => {
-  const myCloud = await cloudinary.v2.uploader.upload(req.body.avatar, {
-    folder: "avatars",
-    width: 150,
-    crop: "scale",
-  });
-
   const { name, email, password } = req.body;
 
   const user = await User.create({
@@ -18,10 +13,12 @@ exports.registerUser = catchAsyncErrors(async (req, res, next) => {
     email,
     password,
     avatar: {
-      public_id: myCloud.public_id,
-      url: myCloud.secure_url,
+      public_id: "myCloud.public_id",
+      url: "myCloud.secure_url",
     },
   });
 
-  sendToken(user, 201, res);
+
+
+  //sendToken(user, 201, res);
 });
