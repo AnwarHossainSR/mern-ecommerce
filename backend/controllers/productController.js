8eb97266c55cff8ef0eba1623c9cf27538053d95
AnwarHossainSR <mahedianwar@gmail.com>
2022-02-02 19:52:11 +0600

    implement filter on product

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -14,7 +14,9 @@ exports.createProduct = catchAsyncErrors(async (req, res, next) => {
 
 //Get All Products
 exports.getAllProducts = catchAsyncErrors(async (req, res, next) => {
-  const apiFeature = new ApiFeatures(Product.find(), req.query).search();
+  const apiFeature = new ApiFeatures(Product.find(), req.query)
+    .search()
+    .filter();
   let products = await apiFeature.query;
   res.status(200).json({
     success: true,
