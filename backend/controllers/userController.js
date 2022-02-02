fd721432f44d0eb1e2f2c979b56fa6a947de3856
AnwarHossainSR <mahedianwar@gmail.com>
2022-02-02 20:33:38 +0600

    create Register method on userController

---
+++ backend/controllers/userController.js
@@ -0,0 +1,27 @@
+const ErrorHander = require("../utils/errorhander");
+const catchAsyncErrors = require("../middleware/catchAsyncErrors");
+const User = require("../models/userModel");
+const crypto = require("crypto");
+
+// Register a User
+exports.registerUser = catchAsyncErrors(async (req, res, next) => {
+  const myCloud = await cloudinary.v2.uploader.upload(req.body.avatar, {
+    folder: "avatars",
+    width: 150,
+    crop: "scale",
+  });
+
+  const { name, email, password } = req.body;
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+    avatar: {
+      public_id: myCloud.public_id,
+      url: myCloud.secure_url,
+    },
+  });
+
+  sendToken(user, 201, res);
+});
