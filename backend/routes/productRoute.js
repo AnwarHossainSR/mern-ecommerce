adccbfe2f778578180591fc10e81f05114b0d89a
AnwarHossainSR <mahedianwar@gmail.com>
2022-02-04 22:33:47 +0600

    added adminRoles middleware

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -6,16 +6,18 @@ const {
   deleteProduct,
   getProductDetails,
 } = require("../controllers/productController");
-const { isAuthenticatedUser } = require("../middleware/auth");
+const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
 
 const router = express.Router();
 
-router.route("/products").get(isAuthenticatedUser, getAllProducts);
-router.route("/product/new").post(isAuthenticatedUser, createProduct);
+router.route("/products").get(getAllProducts);
+router
+  .route("/product/new")
+  .post(isAuthenticatedUser, authorizeRoles("admin"), createProduct);
 router
   .route("/product/:id")
-  .put(isAuthenticatedUser, updateProduct)
-  .delete(isAuthenticatedUser, deleteProduct);
-router.route("/product/:id").get(getProductDetails);
+  .get(getProductDetails)
+  .put(isAuthenticatedUser, authorizeRoles("admin"), updateProduct)
+  .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteProduct);
 
 module.exports = router;
