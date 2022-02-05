de19cb5e83de710566f1799d4dca2358e16531e7
AnwarHossainSR <mahedianwar@gmail.com>
2022-02-05 21:44:29 +0600

    added who am i feature

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -5,6 +5,7 @@ const {
   logout,
   forgotPassword,
   resetPassword,
+  getUserDetails,
 } = require("../controllers/userController");
 const { isAuthenticatedUser } = require("../middleware/auth");
 const router = express.Router();
@@ -14,5 +15,7 @@ router.route("/login").post(loginUser);
 router.route("/logout").get(isAuthenticatedUser, logout);
 router.route("/password/forgot").post(forgotPassword);
 router.route("/password/reset/:token").put(resetPassword);
+
+router.route("/me").get(isAuthenticatedUser, getUserDetails);
 
 module.exports = router;
