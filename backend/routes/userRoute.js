6ea1a79156f0ae2b9400b2d1ca94422a4589828a
AnwarHossainSR <mahedianwar@gmail.com>
2022-02-05 22:20:43 +0600

    Delete User

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -11,6 +11,7 @@ const {
   getAllUser,
   getSingleUser,
   updateUserRole,
+  deleteUser,
 } = require("../controllers/userController");
 const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
 const router = express.Router();
@@ -31,6 +32,7 @@ router
 router
   .route("/admin/user/:id")
   .get(isAuthenticatedUser, authorizeRoles("admin"), getSingleUser)
-  .put(isAuthenticatedUser, authorizeRoles("admin"), updateUserRole);
+  .put(isAuthenticatedUser, authorizeRoles("admin"), updateUserRole)
+  .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteUser);
 
 module.exports = router;
