673eb19af9dc2eca1f839f6674070f07fc69f061
AnwarHossainSR <mahedianwar@gmail.com>
2022-02-13 21:29:44 +0600

    added redux

---
+++ frontend/src/store.js
@@ -0,0 +1,15 @@
+import { applyMiddleware, combineReducers, createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
+const reducers = combineReducers({});
+
+let initialState = {};
+const middleware = [thunk];
+
+const store = createStore(
+  reducers,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
+
+export default store;
