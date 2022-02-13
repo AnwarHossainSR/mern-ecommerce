fba7ca5432602c76fb2fc2370183bdc014b83273
AnwarHossainSR <mahedianwar@gmail.com>
2022-02-13 21:09:38 +0600

    Implemented metadata with helmet

--- frontend/src/component/Home/Home.js
+++ frontend/src/component/Home/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { CgMouse } from "react-icons/cg";
+import MetaData from "../layout/MetaData";
 import "./Home.css";
 import ProductCard from "./ProductCard";
 const product = {
@@ -13,6 +14,7 @@ const Home = () => {
   const iterates = ["Dog", "Bird", "Cat", "Mouse", "Horse", "f", "g", "f"];
   return (
     <>
+      <MetaData title="ECOMMERCE" />
       <div className="banner">
         <p>Welcome to Ecommerce</p>
         <h1>FIND AMAZING PRODUCTS BELOW</h1>
