72d347ea81b2bd07d183675756d8ea6a49037b08
AnwarHossainSR <mahedianwar@gmail.com>
2022-02-23 22:36:39 +0600

    configure react-alert

--- frontend/src/component/Home/Home.js
+++ frontend/src/component/Home/Home.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-//import { useAlert } from "react-alert";
+import { useAlert } from "react-alert";
 import { CgMouse } from "react-icons/cg";
 import { useDispatch, useSelector } from "react-redux";
 import { clearErrors, getProduct } from "../../actions/productAction";
@@ -8,16 +8,16 @@ import MetaData from "../layout/MetaData";
 import "./Home.css";
 import ProductCard from "./ProductCard";
 const Home = () => {
-  // const alert = useAlert();
+   const alert = useAlert();
   const dispatch = useDispatch();
   const { loading, error, products } = useSelector((state) => state.products);
   useEffect(() => {
     if (error) {
-      //alert.error(error);
+      alert.error(error);
       dispatch(clearErrors());
     }
     dispatch(getProduct());
-  }, [dispatch, error]);
+  }, [dispatch, error,alert]);
   return (
     <>
       {loading ? (
